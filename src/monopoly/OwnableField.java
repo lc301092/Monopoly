27abfffb19558cda785ce8e4f05c91d83283db55
rasmniel <rasmniel@gmail.com>
2017-10-18 01:11:16 +0200

    Check this out

--- src/monopoly/OwnableField.java
+++ src/monopoly/OwnableField.java
@@ -6,21 +6,31 @@ package monopoly;
  * @author erso
  */
 public abstract class OwnableField extends Field{
+    // This field is never assigned:
     private String name;
+    
     private int number;
     private int price;
     private Player owner;
 
     protected OwnableField(String name, int number, int price)
     {
+        // Parameter passed to constructor is passed to base class.
         super(number, name);
         this.price = price;
+        // Solution:
+        // this.name = name;
+        
+        // Is this.name redundant? Is this.name semantically equal to Field.fieldName?
+        // In that case, drop this.name and fallback to Field.fieldName!
+        
     }
 
     
 
     @Override
     public String getName(){
+        // TODO Refers instance field of OwnableField!
         return name;
     }
 
